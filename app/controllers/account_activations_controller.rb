81e096ecf810f6beae6e0d06b0078e6fd2a4aed4
Ho Ngoc Doanh <doanhhnqt74@gmail.com>
2016-08-27 16:45:50 +0700

    Add account activations

---
+++ app/controllers/account_activations_controller.rb
@@ -0,0 +1,16 @@
+class AccountActivationsController < ApplicationController
+  
+  def edit
+  	user = User.find_by(email: params[:email])
+  	if user && !user.activated? && user.authenticated?(:activation, params[:id])
+  	  user.activate
+  	  flash[:success] = "Account activated"
+  	  log_in user
+  	  redirect_to user
+  	else
+  	  message = "Invalid activation link"
+  	  flash[:danger] = message
+  	  redirect_to root_url
+  	end
+  end
+end
